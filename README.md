4c296183a97ed61d392e1ab222e044b75f79e415
Jaysh Khan <jayshkhan10@gmail.com>
2024-04-20 20:20:10 +0500

    Matrix Operation Initial Phase

--- README.md
+++ README.md
@@ -1,31 +1,42 @@
 # 5-1 DOF ROT3U kinematics
+
 Forward and inverse Kinematics of 5 DOF + 1 Gripper DOF of ROT3U Robotic Arm
 
 [//]: # (check Box)
+
 - [ ] Forward Kinematics
 - [ ] Inverse Kinematics
 - [ ] Simulation
 - [ ] Hardware Implementation
 
 ## Forward Kinematics
-The forward kinematics of a robot is the method of calculating the Cartesian coordinates of the end-effector from the joint angles of the robot. The forward kinematics of the ROT3U robotic arm is calculated using the Denavit-Hartenberg (DH) parameters.
+
+The forward kinematics of a robot is the method of calculating the Cartesian coordinates of the end-effector from the
+joint angles of the robot. The forward kinematics of the ROT3U robotic arm is calculated using the Denavit-Hartenberg (
+DH) parameters.
 
 ## Inverse Kinematics
-The inverse kinematics of a robot is the method of calculating the joint angles of the robot from the Cartesian coordinates of the end-effector. The inverse kinematics of the ROT3U robotic arm is calculated using the Geometric method.
+
+The inverse kinematics of a robot is the method of calculating the joint angles of the robot from the Cartesian
+coordinates of the end-effector. The inverse kinematics of the ROT3U robotic arm is calculated using the Geometric
+method.
 
 ## Simulation
+
 Still undecided on the simulation software to be used for the simulation of the ROT3U robotic arm.
 
 ## Hardware Implementation
+
 The hardware implementation of the ROT3U robotic arm will be done using the following components:
+
 - ROT3U Robotic Arm
-  - 6 x Servo Motors
-  - 1 x Arduino Uno
-  - 1 x Power Supply
+    - 6 x Servo Motors
+    - 1 x Arduino Uno
+    - 1 x Power Supply
 
-## Documentation 
+## Documentation
+
 - [Documentation Link](https://medium.com/@jayshkhan10/building-and-controlling-a-5-dof-robotic-arm-a-hands-on-guide-with-rot3u-f71c97cfd5fa)
-
 
 [//]: # (Forward and Inverse Kinematics;https://community.robotshop.com/robots/show/xyz-positioning-using-arduino-uno-for-6-dof-robotic-arm&#41)
 
@@ -33,11 +44,12 @@ The hardware implementation of the ROT3U robotic arm will be done using the foll
 
 
 [//]: # (check Box)
+
 - [ ] Forward Kinematics
 - [ ] Inverse Kinematics
 - [ ] Simulation
 
-
 ## references
+
 - [Denavit-Hartenberg (DH) parameters](https://en.wikipedia.org/wiki/Denavit%E2%80%93Hartenberg_parameters)
 - [Geometric method for Inverse Kinematics](https://en.wikipedia.org/wiki/Inverse_kinematics)
---
+++ src/sketch/sketch.ino
@@ -0,0 +1,137 @@
+/*
+ * This code is for controlling the servo motors of ROT3U 5+1 DOF Robotic Arm using the Arduino board.
+ * The code is written in C++ and uses the Arduino library for controlling the servo motors.
+ * @author Jaysh Khan
+*/
+
+#include <Servo.h>
+#include <MatrixMath.h>
+#include <math.h>
+
+#define SERVO_COUNT 1 // number of servos
+#define DELAY 500 // delay between each movement
+#define SERVO_MIN_PWM 500 // minimum pulse width
+#define SERVO_MAX_PWM 2500 // maximum pulse width
+#define SPEED 5 // speed of servo movement
+
+// for Fixing the servo at 0, 90, 180
+int buttonPin0 = 9;
+int buttonPin90 = 10;
+int buttonPin180 = 11;
+int buttonState0=LOW;
+int buttonState90=LOW;
+int buttonState180=LOW;
+//----------------------------
+
+Servo servos[SERVO_COUNT];
+int servo_pin[SERVO_COUNT]={3};//{3,4,5,6,7,8};
+int initialAngles[SERVO_COUNT]={0}; //initializing all with Zero
+int currentAngles[SERVO_COUNT]={0}; //initializing all with Zero
+int angles[SERVO_COUNT] = {0}; //initializing all with Zero
+
+void setup()
+{
+    Serial.begin(9600); //initialize serial communication
+    for (int i=0;i<SERVO_COUNT;i++){
+      servos[i].attach(servo_pin[i],SERVO_MIN_PWM,SERVO_MAX_PWM);
+    }
+    // for Fixing the servo at 0, 90, 180
+    pinMode(buttonPin0, INPUT_PULLUP);
+    pinMode(buttonPin90, INPUT_PULLUP);
+    pinMode(buttonPin180, INPUT_PULLUP);
+}
+
+void loop()
+{
+  // for Fixing the servo at 0, 90, 180
+  buttonState0 = !digitalRead(buttonPin0);
+  buttonState90 = !digitalRead(buttonPin90);
+  buttonState180 = !digitalRead(buttonPin180);
+  Serial.println(buttonState0==HIGH);
+  if (buttonState0 == HIGH) {
+    Serial.println("setting angle 0");
+    angles[0]=0;
+  }
+
+  if (buttonState90 == HIGH) {
+    Serial.println("setting angle 90");
+    angles[0]=90;
+  }
+
+  if (buttonState180 == HIGH) {
+    Serial.println("setting angle 180");
+    angles[0]=180;
+  }
+
+  moveTo(angles);
+  delay(DELAY);
+
+}
+
+/**
+ * Move the servos to the specified angles
+ * @param angles array of angles for each servo
+ * @author Jaysh Khan
+ */
+
+void moveTo( int angles[])
+{
+    for (int i = 0; i < SERVO_COUNT; i++)
+    {
+        // If angle is -1 then the servo should not move
+        if(angles[i] ==-1 || angles[i]==currentAngles[i])
+        {
+            continue;
+        }
+        if(currentAngles[i]<angles[i]){
+          // smoothly moving to that angle
+          for(int j = currentAngles[i];j!=angles[i];j+=SPEED){
+            if (j+SPEED<angles[i]){
+              servos[i].write(j);
+            }
+            else{
+              servos[i].write(angles[i]);
+              break;
+            }
+          }
+        }
+        else {
+          for(int j = currentAngles[i];j!=angles[i];j-=SPEED){
+            if (j-SPEED>angles[i]){
+              servos[i].write(j);
+            }
+            else{
+              servos[i].write(angles[i]);
+              break;
+            }
+            
+          }
+        }
+        
+        currentAngles[i]=angles[i];
+        
+        delay(DELAY);
+    }
+}
+
+/**
+* Reset the servo to initial position
+*/
+
+void reset(){
+  for (int i=0;i<SERVO_COUNT;i++){
+    angles[i]=initialAngles[i];
+  }
+  moveTo(angles);
+}
+
+/**
+ * Transformation Matrix Initializer
+ * @param alpha, theta, a, d
+ * @return 4x4 transformation matrix
+ * @author Jaysh Khan
+ */
+
+ void T_matrix(){
+  
+ }
